250a0422de7f6abad47807617cdfa4b6d9718e6f
schali <admin@florian-m.net>
2012-02-17 15:48:33 +0100

    added test project for testing the release plugin

---
+++ mvn-release-test/src/main/java/com/github/schali/test/mvnreleasetest/App.java
@@ -0,0 +1,13 @@
+package com.github.schali.test.mvnreleasetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
